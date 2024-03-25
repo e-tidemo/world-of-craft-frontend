ccabb771e4d0167361f4654b6c7ada805c08c7a6
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-25 12:45:14 +0100

    Try to make like and unlike post request work

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://world-of-craft-0e06bf8581a1.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.headers.common['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
