5c5917588ed09ed4162a34fd0300118f143bb28a
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-26 11:22:39 +0100

    create an edit page to edit posts

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,7 +5,7 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefault";
 import Post from "./Post";
 
